8777d6f4e61fe108de0548cf4eb601b3c8e9ce0f
Davyd Kurbanov <kyrbanovdavid2004@gmail.com>
2023-10-10 19:47:11 +0200

    update code

--- database.py
+++ database.py
@@ -1,9 +1,6 @@
-from typing import List
-
 import asyncpg
 from asyncpg import UniqueViolationError
-from asyncpg.types import Point
-import json
+
 from config import Config
 from utils import generate_data_types
 import sys
@@ -23,8 +20,6 @@ class DataBase:
         except Exception as e:
             print(f"\tDatabase connection error: {e}")
             sys.exit()
-            
-    
 
     async def create_table(self, cars) -> None:
         async with self.pool.acquire() as connection:
@@ -37,16 +32,44 @@ class DataBase:
                 await connection.execute(query)
             except Exception as _ex:
                 print('\t[ERROR] Creating table failed:\n', _ex)
-                
 
     async def add_car(self, car: dict) -> None:
         async with self.pool.acquire() as connection:
             columns = ", ".join(car.keys())
+            placeholders = ", ".join([f"${i + 1}" for i in range(len(car))])
             values = tuple(car.values())
-            query = f"INSERT INTO car ({columns}) VALUES {values}"
+            query = f"INSERT INTO car ({columns}) VALUES ({placeholders})"
             try:
-                await connection.execute(query)
-            except UniqueViolationError:
-                pass
+                await connection.execute(query, *values)
+            except UniqueViolationError as ex:
+                print(query, values)
+                print(ex)
+            except Exception as ex:
+                print(query, values)
+                print(ex)
+
+    async def add_cars(self, cars: list) -> None:
+        async with self.pool.acquire() as connection:
+            columns = ", ".join(cars[0].keys())
+            values = [tuple(car.values()) for car in cars]
+            placeholders = ", ".join(
+                ["(" + ", ".join(["$" + str(i + 1) for i in range(len(car))]) + ")" for car in values])
+            query = f"INSERT INTO car ({columns}) VALUES {placeholders}"
+            try:
+                await connection.executemany(query, values)
+            except UniqueViolationError as ex:
+                print(query, values)
+                print(ex)
             except Exception as ex:
                 print(query)
+                print(ex)
+
+    import asyncpg
+
+    async def check_links_in_db(self, links: list) -> list:
+        async with self.pool.acquire() as connection:
+            query = "SELECT link FROM car WHERE link = ANY($1)"
+            existing_links = await connection.fetch(query, links)
+            existing_links_set = set(row["link"] for row in existing_links)
+            new_links = [link for link in links if link not in existing_links_set]
+            return new_links
--- parser.py
+++ parser.py
@@ -9,23 +9,27 @@ try:
     from config import Config
     from database import DataBase
     from utils import get_pln_price
+    from fake_useragent import UserAgent
 except ImportError:
     print('Missing modules. Use:\n\tpip install -r requirements.txt')
     sys.exit()
 '''
     TODO:
-1. check currency and exchange all currencies to PLN
-2. closing session
+1. 
+2. 
 3. add to parsing keys 'Wersja', 'Generacja'
 '''
 
 
 
-# URL = "https://www.otomoto.pl/osobowe"
-URL = "https://www.otomoto.pl/osobowe/volkswagen/passat?search%5Bfilter_float_year%3Ato%5D=2010"
 
+URL = "https://www.otomoto.pl/osobowe"
+# URL = "https://www.otomoto.pl/osobowe/volkswagen/passat?search%5Bfilter_float_year%3Ato%5D=2010"
+
+ua = UserAgent()
 HEADERS = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+            # "User-Agent": ua.random,
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -56,10 +60,13 @@ async def get_pages_count(session: ClientSession) -> int:
             else:
                 print(response.status)
                 print(response.headers)
+                if response.status == 403:
+                    await session.close()
+                    print("\tSLEEPING 300 seconds")
+                    time.sleep(300)
 
 
 async def get_cars_link(session: ClientSession, page_number):
-
         request_url = f'?page={page_number}' if not '?' in URL else f'&page={page_number}'
         response = await session.get(URL+request_url)
         if response.status == 200:
@@ -67,10 +74,13 @@ async def get_cars_link(session: ClientSession, page_number):
             soup = BeautifulSoup(html, 'lxml')
             links = soup.find_all('a', href=True)
             html_links = [link['href'] for link in links if link['href'].endswith('.html')]
-            print(f"-\t{page_number}")
+            print(f"-\t{page_number} page")
             return html_links
         else:
             print(response.status)
+            if response.status == 403:
+                print("\tSLEEPING 300 seconds")
+                time.sleep(300)
 
 
 async def get_car_info(session: ClientSession, link, ERROR_LINKS):
@@ -78,7 +88,7 @@ async def get_car_info(session: ClientSession, link, ERROR_LINKS):
         async with session.get(link) as response:
             if response.status == 200:
                 html = await response.text()
-                data_dict = {} 
+                data_dict = {}
                 soup = BeautifulSoup(html, 'lxml')
                 divs = soup.find_all('div', {'data-testid': 'advert-details-item'})
                 for div in divs:
@@ -119,47 +129,72 @@ async def get_car_info(session: ClientSession, link, ERROR_LINKS):
                 data_dict['link'] = link
                 return data_dict
             else:
+                if response.status == 403:
+                    print("\tSLEEPING 300 seconds")
+                    time.sleep(300)
                 ERROR_LINKS.append(link)
     except:
         ERROR_LINKS.append(link)
 
 
 async def main():
+    start_time = time.time()
+    new_items = 0
+
     session = ClientSession(headers=HEADERS)
     try:
         ERROR_LINKS = []
+        ERROR_PAGES = []
         db = DataBase()
         await db.create_pool()
 
-        
-
         pages_count = await get_pages_count(session=session)
+        if not pages_count:
+            session = ClientSession(headers=HEADERS)
+            pages_count = await get_pages_count(session=session)
         print("Pages count:", pages_count)
+
         tasks = []
-        results = []
-        for page in range(1, pages_count+1):
+        for page in range(1100, pages_count+1):
             tasks.append(asyncio.create_task(get_cars_link(session=session, page_number=page)))
-            if page % Config.parse_per_time == 0:
-                results += await asyncio.gather(*tasks)
-                tasks = []
 
-        links = set()
-        for list_links in results:
-            links.update(list_links)
-        links = list(links)
-        print("Links count:", len(links))
+            if page % 20 == 0 or page == pages_count:
+                results = await asyncio.gather(*tasks)
+                set_links = set()
+                for links in results:
+                    if not links:
+                        ERROR_PAGES.append(page)
+                    else:
+                        set_links.update(links)
+                links = list(set_links)
 
-        # GETTING CAR INFO
-        while links:
-            tasks = []
-            for link in links[0:Config.parse_per_time]:
-                tasks.append(get_car_info(session=session, link=link, ERROR_LINKS=ERROR_LINKS))
-            links = links[Config.parse_per_time:]
-            cars = await asyncio.gather(*tasks)
-            await db.create_table(cars)
-            for car in cars:
-                await db.add_car(car=car)
-            print("+\t" + str(Config.parse_per_time))
+                print("\tAll links:", len(links), end=" --- ")
+                links = await db.check_links_in_db(links=links)
+                print("New links:", len(links))
+
+                if links:
+                    # GETTING CAR INFO
+                    tasks = []
+                    for link in links:
+                        tasks.append(get_car_info(session=session, link=link, ERROR_LINKS=ERROR_LINKS))
+                    print("\tParsing cars ...")
+                    cars = await asyncio.gather(*tasks)
+                    tasks = []
+
+                    cars = [car for car in cars if car]
+                    for car in cars:
+                        await db.add_car(car=car)
+                    cars_count = len(cars)
+                    new_items += cars_count
+                    print("+\t" + str(cars_count) + " cars added")
+                    print("\n ------------------------------------------ \n")
+
+                if page % 100 == 0 or page == pages_count:
+                    print(f"\t {new_items} cars was parsed for {time.time() - start_time} seconds")
+                    print("\n ------------------------------------------ \n")
+
+        print(f"\tERROR PAGES: {ERROR_PAGES}")
+        print("\n ------------------------------------------ \n")
 
         # RETRYING ERRORS
         print('Errors:', len(ERROR_LINKS))
